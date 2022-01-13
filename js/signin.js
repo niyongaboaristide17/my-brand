62369c5a7a0e27b8ee46d6c193d6a1acd5f9c4d8
niyongaboaristide17 <49604980+niyongaboaristide17@users.noreply.github.com>
2022-01-13 16:51:44 +0200

    ft:login-page-localstorage-and-validation

---
+++ js/signin.js
@@ -0,0 +1,56 @@
+function checkEmail() {
+    const val = document.getElementById("email-login").value;
+    var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    return re.test(val)
+}
+
+function checkPassword() {
+    // at least one number, one lowercase and one uppercase letter
+    // at least six characters
+    const val = document.getElementById('password-login').value;
+    var re = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+    return re.test(val);
+}
+
+const myCredentials = () => {
+    console.log('Hello Me!');
+    localStorage.setItem("user", JSON.stringify({
+        email: 'niyongaboaristide17@gmail.com',
+        password: '@Password123',
+        isLoggedIn: false
+    }))
+}
+
+myCredentials()
+
+
+function loginSubmit() {
+
+    if (checkEmail() == false && checkPassword() == false) {
+        document.getElementById("email-error").innerHTML = "Invalid email"
+        document.getElementById("password-error").innerHTML = "Invalid password"
+        return false
+    } else if (checkEmail() == false) {
+        document.getElementById("email-error").innerHTML = "Invalid email"
+        return false
+    } else if (checkPassword() == false) {
+        document.getElementById("password-error").innerHTML = "Invalid password"
+        return false
+    }
+
+    const user = JSON.parse(localStorage.getItem("user"))
+    const email = document.getElementById("email-login").value;
+    const password = document.getElementById('password-login').value;
+
+    if (user.email === email && user.password === password) {
+        localStorage.setItem("user", {
+            email: 'niyongaboaristide17@gmail.com',
+            password: '@Password123',
+            isLoggedIn: true
+        })
+        return true
+    } else {
+        return false
+    }
+
+}
